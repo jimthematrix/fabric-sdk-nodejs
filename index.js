18eebbfd832f4fec9f92f7d104a1feca630e1214
Jim Zhang <jzhang@us.ibm.com>
2016-09-13 18:20:07 -0400

    added new classes, modified CA unit tests

--- index.js
+++ index.js
@@ -17,4 +17,46 @@
  * Licensed Materials - Property of IBM
  * © Copyright IBM Corp. 2016
  */
-module.exports = require('./lib/hfc');
+
+var util = require('util');
+var Chain = require('./lib/Chain.js');
+var utils = require('./lib/utils.js');
+
+var _chains = {};
+
+/**
+ * Create a new chain.  If it already exists, throws an Error.
+ * @param name {string} Name of the chain.  It can be any name and has value only for the client.
+ * @returns
+ */
+module.exports.newChain = function(name) {
+    var chain = _chains[name];
+    
+    if (chain) 
+    	throw new Error(util.format("Chain %s already exists", name));
+
+    chain = new Chain(name);
+
+    _chains[name] = chain;
+    return chain;
+}
+
+/**
+ * Get a chain.  If it doesn't yet exist and 'create' is true, create it.
+ * @param {string} chainName The name of the chain to get or create.
+ * @param {boolean} create If the chain doesn't already exist, specifies whether to create it.
+ * @return {Chain} Returns the chain, or null if it doesn't exist and create is false.
+ */
+module.exports.getChain = function(chainName, create) {
+    var chain = _chains[chainName];
+
+    if (!chain && create) {
+        chain = newChain(chainName);
+    }
+
+    return chain;
+}
+
+module.exports.newKeyValueStore = function(path) {
+	return utils.newKeyValueStore(path);
+}
